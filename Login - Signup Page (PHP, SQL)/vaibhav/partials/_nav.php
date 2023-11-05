a414d9259dc09229aa60f39958bf45096946c2a8
thinkyours <130601172+thinkyours@users.noreply.github.com>
2023-11-05 10:56:02 +0530

    Login / Signup page created using XAMPP, PHP, SQL

---
+++ Login - Signup Page (PHP, SQL)/vaibhav/partials/_nav.php
@@ -0,0 +1,28 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="/vaibhav/login.php">SecureShield</a>
+
+            <form class="d-flex ml-auto navbar-form">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/vaibhav/login.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/vaibhav/login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/vaibhav/signup.php">Signup</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
